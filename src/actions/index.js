a8dab4e931a0a53faa0e3d1c0b02febe1f894a46
Muhammad Salehin <xinan2@live.com>
2019-07-22 03:08:06 +0800

    add fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const fetchInitialData = (data) => {
   return (dispatch, getState) => {
-    fetch('../data/tasks.json').then(data => {
+    fetch('https://gist.githubusercontent.com/xinan2/a67ea24bda350697a77ea7daee1b8eef/raw/a008e97361eee1f335c64db7d762451f02b5949c/tasks.json').then(response => {
       console.log('fetch data: ', data);
+      return response.json();
+    }).then(data => {
+      console.log('data: ', data);
     });
     dispatch({
       type: 'FETCH_INITIAL_DATA',
